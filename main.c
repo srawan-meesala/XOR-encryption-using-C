46a25fda2ecd25181ab9d888836461d63e2aa4b4
Srawan Meesala <srawanviz@gmail.com>
2024-11-10 13:41:42 +0530

    version 2 - Added separate functions for encrypt and decrypt and looking for further improvements.

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#define KEY 'X';
+#define KEY '1';
 #define CLEARTEXT 'A';
 
 #include <stdio.h>
@@ -7,6 +7,20 @@ void printchar (char c, char *text)
 {
     printf("%s = '%c' (0x%x)\n", text, c, c);
     return;
+}
+
+char encrypt (char cleartext, char key)
+{
+    char ciphertext;
+    ciphertext = cleartext ^ key;
+    return ciphertext;
+}
+
+char decrypt (char ciphertext, char key)
+{
+    char newcleartext;
+    newcleartext = ciphertext ^ key;
+    return newcleartext;
 }
 
 int main () 
@@ -19,10 +33,10 @@ int main ()
     key = KEY;
     printchar(key, "key");
 
-    ciphertext = cleartext ^ key; /* XOR Operation */
+    ciphertext = encrypt(cleartext, key);
     printchar(ciphertext, "ciphertext");
 
-    newcleartext = ciphertext ^ key; /* XOR Operation */
+    newcleartext = decrypt(ciphertext, key); /* XOR Operation */
     printchar(newcleartext, "newcleartext");
 
     return 0;
